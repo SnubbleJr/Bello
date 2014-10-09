e9a4802a9a8607ae4f8418c76b997749e1315731
Edward James <Edwards_fun@hotmail.com>
2014-10-09 01:15:27 +0100

    Bello?

---
+++ Bello/Assets/SlowlyRotate.cs
@@ -0,0 +1,19 @@
+﻿        using UnityEngine;
+using System.Collections;
+
+public class SlowlyRotate : MonoBehaviour {
+
+	public int speed;
+    public bool x, y;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(x) transform.Rotate(Time.deltaTime * speed, 0, 0);
+        if(y) transform.Rotate(0, Time.deltaTime * speed, 0, Space.World);
+	}
+}
